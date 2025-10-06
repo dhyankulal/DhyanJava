593a93a7b1e024dbb71cd89a45cf4d39c150e2e2
dhyankulal <dhyankulal47@gmail.com>
2025-10-06 05:52:15 +0530

    colour finder

---
+++ ColourFinder.java
@@ -0,0 +1,19 @@
+public class ColourFinder{
+	public static void main(String[] args){
+		String name = "Hamsika";
+		switch(name){
+			case "Arfiya":
+				System.out.println("Apple");
+			break;
+			
+			case "Hamsika":
+				System.out.println("Orange");
+			break;
+			
+			case "shriveni":
+				System.out.println("Banana");
+			break;
+			
+		}
+	}
+}
